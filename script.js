e77b8ec0e76f8a3220d42e5f3e3923924d626949
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2023-12-12 14:17:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	 const alpha=new Map();
+	for(const chars of str){
+		if(alpha.has(chars)){
+			alpha.set(chars,alpha.get(chars)+1);
+		}else{
+			alpha.set(chars,1);
+		}
+	}
+	for(const chars of str){
+		if(alpha.has(chars)){
+			if(alpha.get(chars)==1){
+				return chars;
+			}else{
+				alpha.delete(chars);
+			}
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
